49325fca9d062259a2f1317f58ea750cc2c2d139
Frederik Stensaeth <stensaethf@carleton.edu>
2016-04-08 23:10:44 -0500

    config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "secret":"this app's secret is secret...preferredpicture",
+  "db": process.env.MONGO_URI || "preferredpictureprod:preferredpicture1!@localhost:27017/preferredpicture"
+};
