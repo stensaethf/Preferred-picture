cc56465803667410d6387970c197fe2c12565741
Frederik Stensaeth <stensaethf@carleton.edu>
2016-04-08 23:35:09 -0500

    index route update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Models
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var User = mongoose.model('User');
 
-module.exports = router;
+module.exports = function() {
+  // index.
+  router.get('/', function(req, res) {
+    res.render('index', {}, function(err, html){
+	if (err) {
+		console.log(err);
+	}
+	res.send(html);
+	});
+  });
+
+  return router;
+};
